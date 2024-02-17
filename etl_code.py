5a8f03f34b0cae9c00d8958a176cbd5a91faf597
Oluwajuwon <oyaludejuwon@yahoo.com>
2024-02-17 23:12:59 +0100

    ETL complete. Fix transform function
    The transform function is supposed to be on a copied dataframe of extracted df.

---
+++ etl_code.py
@@ -0,0 +1,73 @@
+import glob
+import pandas as pd
+import xml.etree.ElementTree as ET
+from datetime import datetime
+
+log_file = "log_file.txt"
+target_file = "transformed_data.csv"
+
+def extract_from_csv(file):
+    df = pd.read_csv(file)
+    return df
+
+def extract_from_json(file):
+    df = pd.read_json(file,lines=True)
+    return df
+
+def extract_from_xml(file_to_process): 
+    dataframe = pd.DataFrame(columns=["name", "height", "weight"]) 
+    tree = ET.parse(file_to_process) 
+    root = tree.getroot() 
+    for person in root: 
+        name = person.find("name").text 
+        height = float(person.find("height").text) 
+        weight = float(person.find("weight").text) 
+        dataframe = pd.concat([dataframe, pd.DataFrame([{"name":name, "height":height, "weight":weight}])], ignore_index=True) 
+    return dataframe 
+
+def extract():
+    extracted_data = pd.DataFrame(columns= ['name','height','weight'])
+
+    for csvfile in glob.glob("*.csv"):
+        extracted_data = pd.concat([extracted_data,pd.DataFrame(extract_from_csv(csvfile))], ignore_index = True)
+    
+    for jsonfile in glob.glob('*.json'):
+        extracted_data = pd.concat([extracted_data,pd.DataFrame(extract_from_json(jsonfile))], ignore_index = True)
+
+    for xmlfile in glob.glob('*.xml'):
+        extracted_data = pd.concat([extracted_data,pd.DataFrame(extract_from_xml(xmlfile))], ignore_index= True)
+
+    return extracted_data
+
+
+def transform(data):
+    data['height'] = round(data.height * 0.0254,2)
+    data['weight'] = round(data.weight * 0.45359237,2)
+    return data
+    
+def load_data(target_file, transformed_data):
+    transformed_data.to_csv(target_file)
+
+def log_progress(message):
+    timestamp_format = "%Y-%h-%d-%H:%M:%S"
+    now = datetime.now()
+    timestamp = now.strftime(timestamp_format)
+    with open(log_file,"a") as f:
+        f.write(timestamp + ',' + message + '\n')
+
+log_progress("ETL Job Started")
+
+log_progress("Extract phase started")
+extracted_data = extract()
+
+log_progress("extract phase ended")
+
+log_progress('transform job started')
+transformed_data = transform(extracted_data)
+log_progress('transform phase ended')
+
+log_progress('load phase started')
+load_data(target_file,transformed_data)
+log_progress('load phase ended')
+
+log_progress('ETL job ended')
