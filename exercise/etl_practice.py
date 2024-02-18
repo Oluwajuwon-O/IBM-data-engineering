1a20bad1a01d3ee2db549abb8f79c47bbc1b5747
Oluwajuwon <oyaludejuwon@yahoo.com>
2024-02-18 22:52:20 +0100

    etl_practice.py - in progress

---
+++ exercise/etl_practice.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 18 14:44:29 2024
+
+@author: oyalu
+"""
+import pandas as pd
+import xml.etree.ElementTree as ET
+
+log_file = 'log_file.txt'
+target_file = 'transformed_data.csv'
+
+def extract_csv(file):
+    df = pd.read_csv(file)
+    return df
+
+def extract_json(file):
+    df = pd.read_json(file, lines= True)
+    return df
+
+def extract_xml(file):
+    df = pd.DataFrame(columns=[''])
